729f5319a03e237dfa80d3f885fc8fa0143e92f1
HeeYeonKim98 <wow9896@naver.com>
2020-09-03 19:10:23 +0900

    update databinding

--- Ch2_Jetpack/app/src/main/java/com/example/ch2_jetpack/MainActivity.kt
+++ Ch2_Jetpack/app/src/main/java/com/example/ch2_jetpack/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.ch2_jetpack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
+import com.example.ch2_jetpack.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,28 +17,25 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main) //데이터 바인딩 -> xml에 있는 데이터가 모두 담겨있다.
+        binding.lifecycleOwner=this //livedata를 활용하기 위해 , livedata를 관찰해야
 
         val viewModel=ViewModelProvider(this , ViewModelProvider.AndroidViewModelFactory(application)).get(MainViewModel::class.java)
-//        val viewModel=ViewModelProvider(this)[MainViewModel::class.java]
+        binding.viewModel=viewModel
 
-        viewModel.getAll().observe(this, Observer {
-            ResultText.text=it.toString()
-        })
+
+//        viewModel.getAll().observe(this, Observer {
+//            ResultText.text=it.toString()
+//        })
+        //->databinding을 사용하면 생략가능
 
 //        TodoButton.setOnClickListener {
-//            viewModel.insert(Todo(TodoEditText.text.toString()))
-//            TodoEditText.text.clear()
-//            Toast.makeText(this, "할 일이 추가되었습니다.", Toast.LENGTH_SHORT).show()
+//            lifecycleScope.launch(Dispatchers.IO){
+//                viewModel.insert(TodoEditText.text.toString())
+//                TodoEditText.text.clear()
+//            }
+//
 //        }
-
-        TodoButton.setOnClickListener {
-            lifecycleScope.launch(Dispatchers.IO){
-                viewModel.insert(Todo(TodoEditText.text.toString()))
-            }
-            TodoEditText.text.clear()
-            Toast.makeText(this, "할 일이 추가되었습니다.", Toast.LENGTH_SHORT).show()
-        }
 
     }
 }
--- Ch2_Jetpack/app/src/main/java/com/example/ch2_jetpack/MainViewModel.kt
+++ Ch2_Jetpack/app/src/main/java/com/example/ch2_jetpack/MainViewModel.kt
@@ -3,19 +3,37 @@ package com.example.ch2_jetpack
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import androidx.room.Room
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application){
     private val db = Room.databaseBuilder(
         application,
         TodoDatabase::class.java,"TodoDB"
-    ).allowMainThreadQueries().build()
+    ).build()
+
+    var todos:LiveData<List<Todo>>
+
+    var newTodo:String? = null
+
+    init {
+        todos=getAll()
+    }
 
     fun getAll(): LiveData<List<Todo>> {
         return db.todoDao().getAll()
     }
 
-    suspend fun insert(todo:Todo){
-        db.todoDao().Insert(todo)
+//    suspend fun insert(todo:Todo){
+//        db.todoDao().Insert(todo)
+//    }
+
+    fun insert(todo:String){
+        viewModelScope.launch(Dispatchers.IO) {
+            db.todoDao().Insert(Todo(todo))
+
+        }
     }
 }
